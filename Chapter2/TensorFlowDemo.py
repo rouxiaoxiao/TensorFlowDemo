c1e911fa6f28fb2025ca493aa0f2ee0cdf31106f
xiaoxiao <1621841044@qq.com>
2018-12-04 19:19:35 +0800

    init

---
+++ Chapter2/TensorFlowDemo.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+
+# 使用numpy生成100个随机点
+x_data = np.random.rand(100)
+y_data = x_data * 0.1 + 0.2
+
+# 构建一个线性模型
+b = tf.Variable(0.)
+k = tf.Variable(0.)
+y = k * x_data + b
+
+# 二次代价函数(真实值减预测值，求平方)
+loss = tf.reduce_mean(tf.square(y_data - y))
+# 定义一个梯度下降法来进行训练的优化器
+optimizer = tf.train.GradientDescentOptimizer(0.2)
+# 最小化代价函数
+train = optimizer.minimize(loss)
+
+# 初始化变量
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(201):
+        sess.run(train)
+        if step % 20 == 0:
+            print(step, sess.run([k, b]))
